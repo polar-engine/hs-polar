a97d8361375fc08874848b68cf6ca80ce5938742
Shockk <shokku.ra@gmail.com>
2014-06-24 23:49:28 +0100

    Updated Options

--- src/Polar/Types/Options.hs
+++ src/Polar/Types/Options.hs
@@ -4,22 +4,26 @@ import qualified Polar.Types.Color as C
 import qualified Polar.Types.Callback as CB
 
 data Options = Options
-    { width         :: Int
-    , height        :: Int
-    , title         :: String
-    , swapInterval  :: Int
-    , clearColor    :: C.Color
-    , keyCB         :: Maybe CB.KeyCB
+    { width             :: Int
+    , height            :: Int
+    , title             :: String
+    , swapInterval      :: Int
+    , clearColor        :: C.Color
+    , keyCB             :: Maybe CB.KeyCB
+    , vertexShader      :: String
+    , fragmentShader    :: String
     }
 
 defaultOptions :: Options
 defaultOptions = Options
-    { width         = 1280
-    , height        = 720
-    , title         = "Polar Engine 4"
-    , swapInterval  = 1
-    , clearColor    = C.noColor
-    , keyCB         = Nothing
+    { width             = 1280
+    , height            = 720
+    , title             = "Polar Engine 4"
+    , swapInterval      = 1
+    , clearColor        = C.noColor
+    , keyCB             = Nothing
+    , vertexShader      = "shader"
+    , fragmentShader    = "shader"
     }
 
 dimensions :: Options -> (Int, Int)
