f638fa76dfb82822ca5a03aee6cca9ba00f16474
Shockk <shokku.ra@gmail.com>
2014-06-27 14:42:12 +0100

    Added more colors

--- src/Polar/Types/Color.hs
+++ src/Polar/Types/Color.hs
@@ -16,4 +16,7 @@ colorToGL (Color4 r g b a) = GL.Color4
 noColor         = Color4 0 0 0 0
 blackColor      = Color3 0 0 0
 whiteColor      = Color3 1 1 1
+redColor        = Color3 1 0 0
+greenColor      = Color3 0 1 0
+blueColor       = Color3 0 0 1
 navyBlueColor   = Color3 0.02 0.05 0.1
