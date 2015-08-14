3ee1c69fd05dbc3846d5a574e6d385b16b76b166
Shockk <shokku.ra@gmail.com>
2015-08-14 05:18:33 +0100

    Use applicative style over liftM

--- src/Polar/Renderer/OpenGL_3_2/Shader.hs
+++ src/Polar/Renderer/OpenGL_3_2/Shader.hs
@@ -5,6 +5,7 @@ module Polar.Renderer.OpenGL_3_2.Shader where
 import Data.Maybe (fromMaybe)
 import Data.List (nub, intercalate)
 import qualified Data.Map as M
+import Control.Applicative ((<$>), <*>)
 import Control.Monad.State
 import Polar.Asset.Shader.Tokenizer
 import Polar.Asset.Shader.Parser
@@ -12,14 +13,14 @@ import Polar.Asset.Shader.Types
 
 astComponents :: AST -> State ShaderEnv Int
 astComponents (Assignment name _) = astComponents (Identifier name)
-astComponents (Swizzle asts) = liftM (foldr (+) 0) (mapM astComponents asts)
+astComponents (Swizzle asts) = foldr (+) 0 <$> mapM astComponents asts
 astComponents (Identifier name) = gets currentType >>= \case
     Just Vertex -> case name of
         "position" -> return 4
-        _          -> liftM (M.lookup name) (gets inputs) >>= \case
+        _          -> M.lookup name <$> gets inputs >>= \case
             Just x  -> return x
             Nothing -> unrecognized
-    Just Pixel  -> liftM (M.lookup name) (gets outputs) >>= \case
+    Just Pixel  -> M.lookup name <$> gets outputs >>= \case
         Just x  -> return x
         Nothing -> unrecognized
     Nothing     -> unrecognized
@@ -31,12 +32,12 @@ showName "position" = return "gl_Position"
 showName name = gets currentType >>= \case
     Just Vertex -> case name of
         "position" -> return "gl_Position"
-        _          -> liftM (M.lookup name) (gets inputs) >>= \case
+        _          -> M.lookup name <$> gets inputs >>= \case
             Just _  -> do
                 modify (\env -> env { visitedInputs = name : visitedInputs env })
                 return ("a_" ++ name)
             Nothing -> unrecognized
-    Just Pixel  -> liftM (M.lookup name) (gets outputs) >>= \case
+    Just Pixel  -> M.lookup name <$> gets outputs >>= \case
         Just _  -> do
             modify (\env -> env { visitedOutputs = name : visitedOutputs env })
             return ("o_" ++ name)
@@ -48,28 +49,28 @@ showAST :: AST -> State ShaderEnv String
 showAST (Assignment name ast) = do
     rhs <- showAST ast
     lhs <- showAST (Identifier name)
-    liftM2 (==) (astComponents (Identifier name)) (astComponents ast) >>= \case
+    (==) <$> astComponents (Identifier name) <*> astComponents ast >>= \case
         False -> fail "number of components on lhs does not match number of components on rhs"
         True  -> return ('(' : lhs ++ '=' : rhs ++ ")")
 showAST ast@(Swizzle asts) = do
-    inner <- liftM (intercalate ",") (mapM showAST asts)
+    inner <- intercalate "," <$> mapM showAST asts
     components <- astComponents ast
     return ("(vec" ++ show components ++ '(' : inner ++ "))")
 showAST (Identifier name) = showName name
 showAST (Literal literal) = return (show literal)
 
 showStatement :: AST -> State ShaderEnv String
-showStatement ast = liftM (++ ";") (showAST ast)
+showStatement ast = (++ ";") <$> showAST ast
 
 showFunction :: String -> Maybe String -> State ShaderEnv String
-showFunction name mActualName = liftM (M.lookup name) (gets functions) >>= \case
+showFunction name mActualName = M.lookup name <$> gets functions >>= \case
     Nothing -> fail ("unrecognized function name (" ++ name ++ ")")
     Just asts -> do
         statements <- mapM showStatement asts
         return ("void " ++ fromMaybe name mActualName ++ "(){" ++ concat statements ++ "}")
 
 showIns :: State ShaderEnv [String]
-showIns = liftM nub (gets visitedInputs) >>= f 0
+showIns = nub <$> gets visitedInputs >>= f 0
   where f _ [] = return []
         f n (x:xs) = do
             c <- astComponents (Identifier x)
@@ -77,7 +78,7 @@ showIns = liftM nub (gets visitedInputs) >>= f 0
             return $ ("layout(location=" ++ show n ++ ")in vec" ++ show c ++ " a_" ++ x ++ ";") : rest
 
 showOuts :: State ShaderEnv [String]
-showOuts = liftM nub (gets visitedOutputs) >>= f 0
+showOuts = nub <$> gets visitedOutputs >>= f 0
   where f _ [] = return []
         f n (x:xs) = do
             c <- astComponents (Identifier x)
@@ -88,10 +89,10 @@ showShaders :: State ShaderEnv (String, String)
 showShaders = do
     modify (\env -> env { currentType = Just Vertex, visitedInputs = [] })
     vertex <- showFunction "vertex" (Just "main")
-    ins <- liftM concat showIns
+    ins <- concat <$> showIns
     modify (\env -> env { currentType = Just Pixel, visitedOutputs = [] })
     pixel <- showFunction "pixel" (Just "main")
-    outs <- liftM concat showOuts
+    outs <- concat <$> showOuts
     return ( version ++ vExt ++ precision ++ ins ++ vertex
            , version         ++ precision ++ outs ++ pixel
            )
