2d4d5fb338006a8edf3eae8a2ec17f26ddbf6ec0
Shockk <shokku.ra@gmail.com>
2015-08-21 17:27:32 +0100

    Write more tests for Parser

--- test/Polar/Asset/Shader/ParserSpec.hs
+++ test/Polar/Asset/Shader/ParserSpec.hs
@@ -14,21 +14,47 @@ spec = describe "Parser" $ do
              \[IdentifierT \"a\", BraceOpenT, BraceCloseT]" $
         parse [IdentifierT  "a" , BraceOpenT, BraceCloseT]
         `shouldBe` Right (M.singleton "a" [])
+    it "returns a function with a Literal AST when given \
+             \[IdentifierT \"a\", BraceOpenT, LiteralT 1.0, BraceCloseT]" $
+        parse [IdentifierT  "a" , BraceOpenT, LiteralT 1.0, BraceCloseT]
+        `shouldBe` Right (M.singleton "a" [Literal 1.0])
+    it "returns a function with an Identifier AST when given \
+             \[IdentifierT \"a\", BraceOpenT, IdentifierT \"asdf\", BraceCloseT]" $
+        parse [IdentifierT  "a" , BraceOpenT, IdentifierT  "asdf" , BraceCloseT]
+        `shouldBe` Right (M.singleton "a" [Identifier "asdf"])
+    it "returns a function with a Swizzle AST when given \
+             \[IdentifierT \"a\", BraceOpenT, LiteralT 1.0, LiteralT 2.0, BraceCloseT]" $
+        parse [IdentifierT  "a" , BraceOpenT, LiteralT 1.0, LiteralT 2.0, BraceCloseT]
+        `shouldBe` Right (M.singleton "a" [Swizzle [Literal 1.0, Literal 2.0]])
+    it "returns a function with a Swizzle AST when given \
+             \[IdentifierT \"a\", BraceOpenT, LiteralT 1.0, IdentifierT \"asdf\", IdentifierT \"asdf\", BraceCloseT]" $
+        parse [IdentifierT  "a" , BraceOpenT, LiteralT 1.0, IdentifierT  "asdf" , IdentifierT  "asdf" , BraceCloseT]
+        `shouldBe` Right (M.singleton "a" [Swizzle [Literal 1.0, Identifier "asdf", Identifier "asdf"]])
+    it "returns a function with a Swizzle AST when given \
+             \[IdentifierT \"a\", BraceOpenT, IdentifierT \"asdf\", LiteralT 1.0, LiteralT 2.0, BraceCloseT]" $
+        parse [IdentifierT  "a" , BraceOpenT, IdentifierT  "asdf" , LiteralT 1.0, LiteralT 2.0, BraceCloseT]
+        `shouldBe` Right (M.singleton "a" [Swizzle [Identifier "asdf", Literal 1.0, Literal 2.0]])
+    it "returns a function with an Assignment AST when given \
+             \[IdentifierT \"a\", BraceOpenT, IdentifierT \"var\", EqualsT, IdentifierT \"name\", LiteralT 1.0, LiteralT 2.0, BraceCloseT]" $
+        parse [IdentifierT  "a" , BraceOpenT, IdentifierT  "var" , EqualsT, IdentifierT  "name" , LiteralT 1.0, LiteralT 2.0, BraceCloseT]
+        `shouldBe` Right (M.singleton "a" [Assignment (Identifier "var") $ Swizzle [Identifier "name", Literal 1.0, Literal 2.0]])
     it "returns two empty functions when given \
              \[IdentifierT \"a\", BraceOpenT, BraceCloseT, IdentifierT \"b\", BraceOpenT, BraceCloseT]" $
         parse [IdentifierT  "a" , BraceOpenT, BraceCloseT, IdentifierT  "b" , BraceOpenT, BraceCloseT]
-        `shouldBe` Right (M.fromList
-            [ ("a", [])
-            , ("b", [])
-            ])
+        `shouldBe` Right (M.fromList [("a", []), ("b", [])])
     it "returns two functions with single ASTs when given \
              \[IdentifierT \"a\", BraceOpenT, LiteralT 1.0, BraceCloseT, IdentifierT \"b\", BraceOpenT, LiteralT 2.0, BraceCloseT]" $
         parse [IdentifierT  "a" , BraceOpenT, LiteralT 1.0, BraceCloseT, IdentifierT  "b" , BraceOpenT, LiteralT 2.0, BraceCloseT]
-        `shouldBe` Right (M.fromList
-            [ ("a", [Literal 1.0])
-            , ("b", [Literal 2.0])
-            ])
+        `shouldBe` Right (M.fromList [("a", [Literal 1.0]), ("b", [Literal 2.0])])
     it "returns a singleton function with a single AST when given \
              \[IdentifierT \"a\", BraceOpenT, LiteralT 1.0, BraceCloseT, IdentifierT \"a\", BraceOpenT, LiteralT 2.0, BraceCloseT]" $
         parse [IdentifierT  "a" , BraceOpenT, LiteralT 1.0, BraceCloseT, IdentifierT  "a" , BraceOpenT, LiteralT 2.0, BraceCloseT]
         `shouldBe` Right (M.singleton "a" [Literal 2.0])
+    it "returns a function with two ASTs when given \
+             \[IdentifierT \"a\", BraceOpenT, LiteralT 1.0, StatementEndT, LiteralT 2.0, BraceCloseT]" $
+        parse [IdentifierT  "a" , BraceOpenT, LiteralT 1.0, StatementEndT, LiteralT 2.0, BraceCloseT]
+        `shouldBe` Right (M.singleton "a" [Literal 1.0, Literal 2.0])
+    it "returns a function with two ASTs when given \
+             \[IdentifierT \"a\", BraceOpenT, LiteralT 1.0, NewLineT, LiteralT 2.0, BraceCloseT]" $
+        parse [IdentifierT  "a" , BraceOpenT, LiteralT 1.0, NewLineT, LiteralT 2.0, BraceCloseT]
+        `shouldBe` Right (M.singleton "a" [Literal 1.0, Literal 2.0])
