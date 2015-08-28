5920aee00be4803f2a7ec5d92a16e3f6e2825e53
Shockk <shokku.ra@gmail.com>
2015-08-28 10:57:10 +0100

    Use lens mappend operator thing (monoids are nice)

--- src/Polar/Listener.hs
+++ src/Polar/Listener.hs
@@ -3,11 +3,11 @@
 module Polar.Listener where
 
 import Data.Maybe (fromMaybe)
-import Control.Lens ((%=), use, at)
+import Control.Lens ((<>=), use, at)
 import Polar.Types
 
 listen :: Event -> Listener -> PolarIO ()
-listen event listener = listeners . at event %= Just . (++ [listener]) . fromMaybe []
+listen event listener = listeners . at event <>= Just [listener]
 
 notify :: Event -> Notification -> PolarIO ()
 notify event note = use (listeners . at event) >>= mapM_ ($ note) . fromMaybe []
