9ff4fb531c7cf9c79b2abbaea505d6e632eddd61
Shockk <shokku.ra@gmail.com>
2015-08-29 10:12:15 +0100

    Setup buffer modes in Logger instead of Engine

--- src/Polar/Engine.hs
+++ src/Polar/Engine.hs
@@ -3,17 +3,13 @@
 module Polar.Engine where
 
 import Data.Function.Apply
-import Control.Monad.State
+import Control.Monad (unless)
 import Control.Lens (use)
-import System.IO (hSetBuffering, stdout, stderr, BufferMode(..))
 import Polar.Types
 import Polar.Listener
 
 run :: PolarIO ()
 run = do
-    liftIO $ do
-        hSetBuffering stdout NoBuffering
-        hSetBuffering stderr LineBuffering
     use startup >>= mapM_ (listen StartupNote)
     notify StartupNote ()
     loop
--- src/Polar/Logger.hs
+++ src/Polar/Logger.hs
@@ -1,13 +1,16 @@
 module Polar.Logger where
 
 import Control.Monad.State (liftIO)
-import System.IO (stderr, hPutStrLn)
+import System.IO (BufferMode(..), stdout, stderr, hPutStrLn, hSetBuffering)
 import GHC.Stack (currentCallStack, renderStack)
 import Polar.Types
 import Polar.Listener
 
 startup :: ListenerF ()
-startup _ _ = listen ErrorNote (Listener onError)
+startup _ _ = do
+    liftIO (hSetBuffering stdout NoBuffering)
+    liftIO (hSetBuffering stderr LineBuffering)
+    listen ErrorNote (Listener onError)
 
 onError :: ListenerF String
 onError ErrorNote err = do
