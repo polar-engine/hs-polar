9c6bea547d5ba93a53a412806dec1b75313f1db5
Shockk <shokku.ra@gmail.com>
2015-09-13 04:53:27 +0100

    Polar.Listener: Write haddock documentation

--- src/Polar/Listener.hs
+++ src/Polar/Listener.hs
@@ -1,6 +1,14 @@
 {-# LANGUAGE FlexibleContexts #-}
 
-module Polar.Listener where
+{-|
+  Module      : Polar.Listener
+  Stability   : unstable
+  Portability : non-portable (GHC Extensions)
+
+  Helper functions to perform common operations on engine event listeners.
+-}
+
+module Polar.Listener (listen, notify, notify') where
 
 import Data.Maybe (fromMaybe)
 import Data.Typeable (Typeable)
@@ -9,13 +17,26 @@ import Control.Monad (when)
 import Control.Lens ((<>=), use, at)
 import Polar.Types
 
-listen :: MonadPolarState m => String -> Listener -> m ()
+-- |Insert a listener into the engine.
+listen :: MonadPolarState m
+       => String   -- ^ event name
+       -> Listener -- ^ listener
+       -> m ()
 listen note listener = listeners . at note <>= Just [listener]
 
-notify :: Typeable a => String -> a -> PolarIO ()
+-- |Dispatch an event to all listeners.
+notify :: Typeable a
+       => String -- ^ event name
+       -> a      -- ^ argument
+       -> PolarIO ()
 notify = notify' 1
 
-notify' :: Typeable a => Int -> String -> a -> PolarIO ()
+-- |Dispatch an event to all listeners with a maximum error notification depth.
+notify' :: Typeable a
+        => Int    -- ^ maximum error notification depth
+        -> String -- ^ event name
+        -> a      -- ^ argument
+        -> PolarIO ()
 notify' n note x = use (listeners . at note) >>= mapM_ notifyOne . fromMaybe []
   where notifyOne (Listener f) = case fromDynamic (toDyn f) of
             Nothing -> when (n > 0) $ notify' (pred n) "error" ("type mismatch in " ++ show note)
