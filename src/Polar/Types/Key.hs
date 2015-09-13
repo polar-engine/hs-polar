dd4a3c9e86449d4fd8e9e0994ad8a620fa85bb1f
Shockk <shokku.ra@gmail.com>
2015-09-13 16:19:31 +0100

    Polar.Types.Key: Write Haddock documentation

--- src/Polar/Types/Key.hs
+++ src/Polar/Types/Key.hs
@@ -1,7 +1,21 @@
 {-# LANGUAGE Safe #-}
 
+{-|
+  Module      : Polar.Types.Key
+  Copyright   : (c) 2015 David Farrell
+  License     : Apache-2.0
+  Stability   : unstable
+  Portability : portable
+
+  This module defines keyboard data types.
+-}
+
 module Polar.Types.Key where
 
+-- |Wrapper type for keyboard events.
+data KeyEvent = KeyEvent Key KeyAction KeyModifiers
+
+-- |Sources of keyboard input.
 data Key = Key1 | Key2 | Key3 | Key4 | Key5 | Key6 | Key7 | Key8 | Key9 | Key0
          | KeyA | KeyB | KeyC | KeyD | KeyE | KeyF | KeyG | KeyH | KeyI | KeyJ
          | KeyK | KeyL | KeyM | KeyN | KeyO | KeyP | KeyQ | KeyR | KeyS | KeyT
@@ -15,8 +29,10 @@ data Key = Key1 | Key2 | Key3 | Key4 | Key5 | Key6 | Key7 | Key8 | Key9 | Key0
          | KeyF9 | KeyF10 | KeyF11 | KeyF12
          | KeyUnknown
 
+-- |Keyboard actions.
 data KeyAction = KeyDownAction | KeyUpAction
 
+-- |Keyboard modifiers.
 data KeyModifiers = KeyModifiers
     { shiftModifier :: Bool
     , ctrlModifier  :: Bool
@@ -24,7 +40,6 @@ data KeyModifiers = KeyModifiers
     , superModifier :: Bool
     }
 
+-- |Default value for 'KeyModifiers'
 defaultKeyModifiers :: KeyModifiers
 defaultKeyModifiers = KeyModifiers False False False False
-
-data KeyEvent = KeyEvent Key KeyAction KeyModifiers
