700337cddb9095be68da6300925bc6df76dd0d26
Shockk <shokku.ra@gmail.com>
2015-09-13 15:18:34 +0100

    Polar.Control: Write Haddock documentation

--- src/Polar/Control.hs
+++ src/Polar/Control.hs
@@ -1,9 +1,20 @@
 {-# LANGUAGE Safe #-}
 
-module Polar.Control where
+{-|
+  Module      : Polar.Control
+  Copyright   : (c) 2015 David Farrell
+  License     : Apache-2.0
+  Stability   : unstable
+  Portability : portable
+
+  Helper functions to control the functioning of the engine.
+-}
+
+module Polar.Control (exit) where
 
 import Control.Lens ((.=))
 import Polar.Types
 
+-- |Exit out of the engine after the current tick.
 exit :: PolarIO ()
 exit = willExit .= True
