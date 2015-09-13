22c5db44760ab9002f25a06efc1254241703db49
Shockk <shokku.ra@gmail.com>
2015-09-13 16:09:10 +0100

    Polar.Types: Write Haddock documentation

--- src/Polar/Types.hs
+++ src/Polar/Types.hs
@@ -1,5 +1,15 @@
 {-# LANGUAGE Safe #-}
 
+{-|
+  Module      : Polar.Types
+  Copyright   : (c) 2015 David Farrell
+  License     : Apache-2.0
+  Stability   : unstable
+  Portability : portable
+
+  This module combines all type and lens modules for convenience.
+-}
+
 module Polar.Types
 ( module Polar.Types.Point
 , module Polar.Types.Box
