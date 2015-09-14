9a7aa16c656b6bcefc41c1cf1ea35dbb15722401
Shockk <shokku.ra@gmail.com>
2015-09-14 06:09:09 +0100

    Point: Rewrite magnitude as a lens

--- src/Polar/Types/Point.hs
+++ src/Polar/Types/Point.hs
@@ -14,6 +14,8 @@
 
 module Polar.Types.Point where
 
+import Control.Lens (Lens')
+
 -- |Representation of a 4-component point with x, y, z, and weight.
 data Point a = Point { _pointX :: a -- ^ x component
                      , _pointY :: a -- ^ y component
@@ -21,7 +23,7 @@ data Point a = Point { _pointX :: a -- ^ x component
                      , _pointW :: a -- ^ weight component
                      } deriving (Eq, Show, Functor, Foldable)
 
--- |Map numeric operations over all components.
+-- |Perform numeric operations on x, y, and z components.
 instance Num a => Num (Point a) where
     (Point x1 y1 z1 w1) + (Point x2 y2 z2 w2) = Point (x1 * w1 + x2 * w2) (y1 * w1 + y2 * w2) (z1 * w1 + z2 * w2) 1
     (Point x1 y1 z1 w1) * (Point x2 y2 z2 w2) = Point (x1 * w1 * x2 * w2) (y1 * w1 * y2 * w2) (z1 * w1 * z2 * w2) 1
@@ -35,9 +37,7 @@ instance Num a => Num (Point a) where
 defaultPoint :: Num a => Point a
 defaultPoint = fromInteger 0
 
--- TODO: make HasMagnitude somehow using Control.Lens.TH
--- |Calculate the scalar magnitude of a 'Point'
-magnitude :: RealFloat a
-          => Point a -- ^ point
-          -> a
-magnitude (Point x y z w) = sqrt (x * x + y * y + z * z) * w
+-- |Lens into the scalar magnitude of a 'Point'.
+magnitude :: (Eq a, Floating a) => Lens' (Point a) a
+magnitude f (Point x y z 1) = Point x y z <$> f (sqrt (x * x + y * y + z * z))
+magnitude f (Point x y z w) = magnitude f (Point (x * w) (y * w) (z * w) 1)
