e10ac719dddd4e8ea7d0a4b83944b1f366b116ca
Shockk <shokku.ra@gmail.com>
2015-09-14 02:57:09 +0100

    Polar.Types.Engine: Write Haddock documentation

--- src/Polar/Types/Engine.hs
+++ src/Polar/Types/Engine.hs
@@ -2,6 +2,16 @@
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE ExistentialQuantification #-}
 
+{-|
+  Module      : Polar.Types.Engine
+  Copyright   : (c) 2015 David Farrell
+  License     : Apache-2.0
+  Stability   : unstable
+  Portability : non-portable (GHC extensions)
+
+  This module defines the core engine types.
+-}
+
 module Polar.Types.Engine where
 
 import Data.Typeable (Typeable)
@@ -10,13 +20,22 @@ import Control.Monad.State
 import Polar.Types.Point
 import Polar.Types.Box
 
+-- |Alias to 'MonadState' 'Engine' for conveneince.
 type MonadPolar = MonadState Engine
+
+-- |Pure engine monad.
 type Polar = State Engine
+
+-- |Engine monad transformed with an inner monad of 'IO'.
 type PolarIO = StateT Engine IO
 
+-- |Event listener function.
 type ListenerF a = String -> a -> PolarIO ()
+
+-- |Wrapper around 'ListenerF' existentially quantified by the listener argument type.
 data Listener = forall a. Typeable a => Listener (ListenerF a)
 
+-- |Engine state environment.
 data Engine = Engine { _engineTitle     :: String
                      , _engineStartup   :: [Listener]
                      , _engineListeners :: M.Map String [Listener]
@@ -24,6 +43,7 @@ data Engine = Engine { _engineTitle     :: String
                      , _engineViewport  :: Box Int
                      }
 
+-- |Default value for 'Engine'.
 defaultEngine :: Engine
 defaultEngine = Engine
     { _engineTitle     = "Polar Engine 4"
