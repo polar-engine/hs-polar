ab455a2cd7e88fc04c2b11af0e48d003230fa859
Shockk <shokku.ra@gmail.com>
2015-10-05 12:31:29 +0100

    Build example default config

---
+++ src/Polar/Types/Config.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE Safe #-}
+
+{-|
+  Module      : Polar.Types.Config
+  Copyright   : (c) 2015 David Farrell
+  License     : Apache-2.0
+  Stability   : unstable
+  Portability : portable
+
+  Engine configuration types.
+-}
+
+module Polar.Types.Config where
+
+import Polar.ConfigFile
+
+defaultConfig = emptyCP |%| "Core"     |$| "TimeToSleep"  |=| "10"
+                        |%| "Renderer" |$| "FieldOfView"  |=| "70"
+                                       |$| "ViewDistance" |=| "1000"
+                                       |$| "SSAO"         |=| "yes"
+                        |%| "Mixer"    |$| "Muted"        |=| "no"
