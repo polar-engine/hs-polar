3087101251f18f1a451ade87d7b535e0904f17b0
Shockk <shokku.ra@gmail.com>
2016-08-01 23:21:13 +0100

    Renderer: Clear color to (0,0,0,0)

--- src/Polar/System/Renderer/OpenGL_3_2.hs
+++ src/Polar/System/Renderer/OpenGL_3_2.hs
@@ -13,7 +13,10 @@
 
 module Polar.System.Renderer.OpenGL_3_2 (renderer) where
 
-import Control.Monad.RWS (liftIO, tell)
+import Data.Foldable (traverse_)
+import Control.Monad.RWS (MonadIO, liftIO, tell)
+import Graphics.Rendering.OpenGL (($=))
+import qualified Graphics.Rendering.OpenGL as GL
 import qualified Graphics.UI.GLFW as GLFW
 import Polar.Types
 import Polar.Log
@@ -21,14 +24,12 @@ import Polar.Storage
 
 renderer :: System
 renderer = defaultSystem "OpenGL 3.2 Renderer"
-    & startup  .~ startupF
-    & shutdown .~ shutdownF
+    & startup  .~ tell [SysCoreAction startupF]
+    & tick     .~ tell [SysCoreAction tickF]
+    & shutdown .~ tell [SysCoreAction shutdownF]
 
-startupF :: Sys ()
-startupF = tell [SysCoreAction startupWindow]
-
-startupWindow :: Core ()
-startupWindow = do
+startupF :: Core ()
+startupF = do
     liftIO $ do
         GLFW.init
         GLFW.windowHint (GLFW.WindowHint'ContextVersionMajor 3)
@@ -41,12 +42,24 @@ startupWindow = do
             logWrite DEBUG "Created window"
             liftIO $ GLFW.makeContextCurrent (Just win)
             store win "window"
+            gl (GL.clearColor $= GL.Color4 0 0 0 0)
 
-shutdownF :: Sys ()
-shutdownF = tell [SysCoreAction shutdownWindow]
+tickF :: Core ()
+tickF = do
+    gl (GL.clear [GL.ColorBuffer, GL.DepthBuffer])
+    liftIO . GLFW.swapBuffers =<< forceRetrieve as "window"
+    liftIO GLFW.pollEvents
 
-shutdownWindow :: Core ()
-shutdownWindow = do
+shutdownF :: Core ()
+shutdownF = do
     liftIO . GLFW.destroyWindow =<< forceRetrieve as "window"
     logWrite DEBUG "Destroyed window"
     liftIO GLFW.terminate
+
+gl :: (MonadIO m, LogPolar m) => IO a -> m a
+gl io = do
+    result <- liftIO io
+    traverse_ logGLError =<< liftIO (GL.get GL.errors)
+    pure result
+  where logGLError (GL.Error category msg) = logWrite CRITICAL $
+            "OpenGL error (Category = " ++ show category ++ ", " ++ msg ++ ")"
