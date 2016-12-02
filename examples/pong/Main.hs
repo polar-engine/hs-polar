67cb04d22339ee8ea6f5a6cff9d8ac3910b8e5d6
David Farrell <shokku.ra@gmail.com>
2016-12-02 10:06:49 +0000

    Update pong example

--- examples/pong/Main.hs
+++ examples/pong/Main.hs
@@ -1,11 +1,33 @@
 module Main where
 
+import Data.Typeable
 import Polar
 
+-- Polar
+
+(<->) :: (Typeable o, Typeable c) => o -> c -> Core ()
+_ <-> _ = undefined
+
+data Sprite = Sprite String
+data Phys = StaticPhys | BouncePhys Rational
+data Bounds = SpriteBounds | ViewportBounds | InvertedBounds Bounds
+
+data Viewport = Viewport
 data Ball = Ball
 data Paddle = LeftPaddle | RightPaddle
 
 logic = do
-    Ball        <=> [SpriteBoxBounds,   BouncePhys, CenterAnchor, Sprite "ball"]
-    LeftPaddle  <=> [SpriteYAxisBounds, StaticPhys, LeftAnchor,   Sprite "paddle"]
-    RightPaddle <=> [SpriteYAxisBounds, StaticPhys, RightAnchor,  Sprite "paddle"]
+    Viewport <-> StaticPhys
+    Viewport <-> InvertedBounds ViewportBounds
+    Ball <-> BouncePhys 1
+    Ball <-> SpriteBounds
+    Ball <-> Sprite "ball"
+    LeftPaddle <-> StaticPhys
+    LeftPaddle <-> SpriteBounds
+    LeftPaddle <-> Sprite "paddle"
+    RightPaddle <-> StaticPhys
+    RightPaddle <-> SpriteBounds
+    RightPaddle <-> Sprite "paddle"
+    --on (Ball `CollideX` Viewport) reset
+
+main = run $ defaultEngine
